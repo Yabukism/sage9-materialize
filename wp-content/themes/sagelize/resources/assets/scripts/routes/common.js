74f9097d32b1930f268454af9a1753d522a3f31c
yabuki <yabuki@webcreators.info>
2018-09-08 20:05:50 +0900

    materializecssのjs 読み込まない。bootstrap に統一する方向で。

--- wp-content/themes/sagelize/resources/assets/scripts/routes/common.js
+++ wp-content/themes/sagelize/resources/assets/scripts/routes/common.js
@@ -1,6 +1,9 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $(".nav-item-dropdown-button").dropdown({constrainWidth: true});
+    $(".side-menu-nav-item-dropdown-button").dropdown({constrainWidth: false});
+    $(".button-collapse").sideNav();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
---
+++ wp-content/themes/sagelize/resources/views/partials/sidenav.php
@@ -0,0 +1,24 @@
+<ul id="slide-out" class="sidenav">
+  <li>
+    <a class="waves-effect waves-light" href="<?php echo esc_url(home_url('/%E6%97%A5%E6%9C%AC%E5%8F%B2%E3%83%BB%E4%B8%96%E7%95%8C%E5%8F%B2%EF%BC%88%E7%9B%AE%E6%AC%A1%EF%BC%89/')); ?>"><i class="material-icons">school</i>日本史/世界史</a>
+  </li>
+  <li>
+    <a class="waves-effect waves-light" href="<?php echo esc_url(home_url('/timeline/')); ?>"><i class="material-icons">account_balance</i>年表から探す</a>
+  </li>
+  <li>
+    <a class="waves-effect waves-light" href="<?php echo esc_url(home_url('/map-search')); ?>/"><i class="material-icons">add_location</i>地図から探す</a>
+  </li>
+  <li>
+    <a class="waves-effect waves-light" href="<?php echo esc_url(home_url('/keywords-lists/')); ?>"><i class="material-icons">book</i>用語集</a>
+  </li>
+  <li>
+    <a class="waves-effect waves-light" href="<?php echo esc_url(home_url('/chronology/')); ?>"><i class="material-icons">timeline</i>年表・表</a>
+  </li>
+  <li>
+    <a class="waves-effect waves-light" href="<?php echo esc_url(home_url('/downloads-lists/')); ?>"><i class="material-icons pink-text text-lighten-3">cloud_download</i>ダウンロード</a>
+  </li>
+  <li class="search">
+    <?php //get_search_form();?>
+  </li>
+</ul>
+<a href="#" data-target="slide-out" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a>
